4b336d08d193a288fb32049c73493c8994a7f039
FlavioRoman <romanflavio3@hotmail.com>
2025-10-04 11:17:47 -0300

    Implement form data handling and JSON response for /api/data endpoint

--- app.js
+++ app.js
@@ -35,7 +35,26 @@ app.get("/search", (req, res) => {
 });
 
 // Para recibir la informacion.
-app.post("/form", (req, res) => {});
+app.post("/form", (req, res) => {
+  const name = req.body.nombre || "Anonimo";
+  const email = req.body.email || "No proporcionado";
+  res.json({
+    message: "Datos recibidos",
+    data: {
+      name,
+      email,
+    },
+  });
+});
+
+app.post("/api/data", (req, res) => {
+  const data = req.body;
+  if (!data || Object.keys(data).length === 0) {
+    return res.status(400).json({ error: "No se recibieron datos" });
+  }
+
+  res.status(201).json({ message: "Datos JSON recibidos", data });
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
